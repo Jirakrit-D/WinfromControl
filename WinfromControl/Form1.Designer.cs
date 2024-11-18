3bcaa748a52e87b0affcfe0cdc001b8a20ef138a
นายอภิรักษ์ ศรีพุทธา <65040249103@udru.ac.th>
2024-11-18 11:54:54 +0700

    Add project files.

---
+++ WinfromControl/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace WinfromControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(805, 62);
+            button1.Name = "button1";
+            button1.Size = new Size(134, 62);
+            button1.TabIndex = 0;
+            button1.Text = "ปิดโปรดเแกรม";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(570, 62);
+            button2.Name = "button2";
+            button2.Size = new Size(115, 62);
+            button2.TabIndex = 1;
+            button2.Text = "เปิดไฟ";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            button2.MouseLeave += button2_MouseLeave;
+            button2.MouseHover += button2_MouseHover;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(691, 62);
+            button3.Name = "button3";
+            button3.Size = new Size(108, 62);
+            button3.TabIndex = 2;
+            button3.Text = "ปิดไฟ";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // label1
+            // 
+            label1.BackColor = Color.Cyan;
+            label1.Font = new Font("Angsana New", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ActiveCaptionText;
+            label1.Location = new Point(62, 72);
+            label1.Name = "label1";
+            label1.Size = new Size(382, 128);
+            label1.TabIndex = 3;
+            label1.Text = "มหาวิทยาลัยราชภัฎอุดรธาณี";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            label1.Click += label1_Click;
+            label1.MouseLeave += label1_MouseLeave;
+            label1.MouseHover += label1_MouseHover;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.Aquamarine;
+            textBox1.Location = new Point(62, 250);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(382, 130);
+            textBox1.TabIndex = 4;
+            textBox1.Text = "Your Open the door";
+            textBox1.TextChanged += textBox1_TextChanged;
+            textBox1.DoubleClick += textBox1_DoubleClick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.GradientInactiveCaption;
+            ClientSize = new Size(1016, 502);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "WindowFrom ";
+            FormClosing += Form1_FormClosing;
+            Load += Form1_Load;
+            Click += Form1_Click;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label1;
+        private TextBox textBox1;
+    }
+}
